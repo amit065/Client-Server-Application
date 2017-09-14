b8b05fa7d6cd1bd00c27ae2e2b934e8bc677e315
Amit Prakash <aprakash@internal.tavisca.com>
2017-09-14 19:08:57 +0530

    client server application

---
+++ Server/Server/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public string LastName { get; set; }
+
+
+        public Person(string first, string second, string last)
+        {
+            FirstName = first;
+            MiddleName = second;
+            LastName = last;
+        }
+    }
+}
